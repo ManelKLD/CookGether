fe8fd0a4335a7943f53f7d1b33b93791e9b38c34
Manel <manelkld369@gmail.com>
2022-03-15 13:39:17 +0100

    Lien accueil et connexion

--- app/src/main/java/com/example/cookgether/MainActivity.java
+++ app/src/main/java/com/example/cookgether/MainActivity.java
@@ -2,15 +2,44 @@ package com.example.cookgether;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity   {
+
+    private Button btnCon;
+    private Button btnIns;
+    private Intent conIntent;
+    private Intent insIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //
+        btnCon = (Button) findViewById(R.id.btnCon);
+
+        btnIns = (Button) findViewById(R.id.btnIns);
+    }
+
+
+
+    public void identification(View view){
+        switch (view.getId()) {
+            case R.id.btnCon: {
+                conIntent = new Intent(MainActivity.this,connexionActivity.class);
+                startActivity(conIntent);
+                break;
+            }
+            case R.id.btnIns: {
+                insIntent = new Intent(MainActivity.this,InscriptionActivity.class);
+                startActivity(insIntent);
+                break;
+            }
+        }
+
     }
 }
